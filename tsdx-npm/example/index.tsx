3864e3a77334288d4fcf1179b3ec0f715b5fb82b
Neider Galofre Morales <desarrollador2@unicurn.edu.co>
2023-07-10 08:58:59 -0500

    CODE BASE

---
+++ tsdx-npm/example/index.tsx
@@ -0,0 +1,21 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import GoogleLogin from './..';
+
+const App = () => {
+  return (
+    <div style={{ display: "grid", placeContent: "center", height: "90vh" }}>
+      <div style={{ maxWidth: "300px", display: "grid", placeContent: "center" }}>
+        <GoogleLogin
+          client_id='your_client_id_here'
+          successCallback={({ credendial, select_by }) => {
+            console.log(credendial, select_by);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
